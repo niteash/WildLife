1b95e6c8808073a488fc328e8691ae4451973c2b
Shwe Win Aung <79239625+niteash@users.noreply.github.com>
2022-08-27 14:01:04 +0630

    Delete Myanmar map directory

--- Myanmar map/Myanmarmap.js
+++
@@ -1,9 +0,0 @@
-let CityName = document.querySelector('.current-place');
-let AllPath = document.querySelectorAll('.map path');
-
-AllPath.forEach(function(x){
-    x.addEventListener("mouseover",function(){
-    CityName.innerText = x.getAttribute('title');
-        
-    });
-})
